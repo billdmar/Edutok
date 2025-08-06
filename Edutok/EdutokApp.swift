6dc47196a48c224029d4ff0405affcdc4a2cf785
William Mar <billdmar@gmail.com>
2025-08-05 19:02:30 -0500

    Initial Commit

---
+++ Edutok/EdutokApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutokApp.swift
+//  Edutok
+//
+//  Created by Bill Mar on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
